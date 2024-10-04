9a9b695bd398712418bb4da8262d19fdb98db1cf
Tushar Singh <viptusharsingh2000@gmail.com>
2024-10-04 13:58:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const MongoStore = require('connect-mongo');
 const bcrypt = require('bcryptjs');
 const connectDatabase = require('./DB');
 const Users = require('./userModel');
+const Message = require('./messageModel');
 require('dotenv').config();
 
 const app = express();
@@ -37,9 +38,6 @@ app.use(express.urlencoded({ extended: true }));
 // Connect to database
 connectDatabase();
 
-// Store messages for the chat session
-const messages = [];
-
 // User registration endpoint
 app.post('/register', async (req, res) => {
     const { username, password, email, number } = req.body;
@@ -58,7 +56,6 @@ app.post('/register', async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = new Users({ name: username, password: hashedPassword, email, number });
         await newUser.save();
-
         return res.status(200).json({ success: true, message: 'User registered successfully.' });
 
     } catch (error) {
@@ -83,8 +80,7 @@ io.on('connection', (socket) => {
                 const username = userData.name;
                 socket.request.session.username = username;
                 socket.request.session.save();
-                socket.emit('login response', { success: true });
-                io.emit('user status', { message: `${username} has joined the chat.` });
+                socket.emit('login response', { success: true, username });
             } else {
                 socket.emit('login response', { success: false, message: 'Incorrect Password, Please Try Again!' });
             }
@@ -94,19 +90,61 @@ io.on('connection', (socket) => {
         }
     });
     
+    socket.on('join chat', (data) => {
+        io.emit('user joined', { message: `${data.username} has joined the chat.` });
+        console.log(`${data.username} has joined the chat.`)
+    });
+
+    socket.on('chat message', async (data) => {
+        try {
+            const newMessage = new Message({ name: data.name, message: data.message, time: data.time});
+            const savedMessage = await newMessage.save();
+            const messageData = {
+                name: savedMessage.name,
+                message: savedMessage.message,
+                time: savedMessage.time
+            };
+    
+            io.emit('display message', messageData);
+        } catch (error) {
+            console.error(error);
+        }
+    });
+    
+
     socket.on('logout user', (data) => {
-        io.emit('user status', { message: `${data.username} has logged out.` });
+        console.log(`${data.username} has logged out.`)
+        // io.emit('user status', { message: `${data.username} has logged out.` });
         socket.request.session.destroy();
     });
 
-    socket.on('retrieve messages', () => {
-        socket.emit('message history', messages);
+    socket.on('retrieve messages', async () => {
+        try {
+            const messages = await Message.find({}).sort({ createdAt: -1 });
+            socket.emit('all messages', messages);
+        } catch (error) {
+            console.error('Error retrieving messages:', error);
+        }
     });
 
-    socket.on('chat message', (msg) => {
-        messages.push(msg);
-        io.emit('chat message', msg);
-    });
+
+    socket.on('clear Database', async () =>{
+        try {
+            await Users.deleteMany({});
+            console.log('Old users removed successfully.');
+        } catch (error) {
+            console.error('Error removing old users:', error);
+        }
+    })
+
+    socket.on('clear Message Database', async () =>{
+        try {
+            await Message.deleteMany({});
+            console.log('Old Message removed successfully.');
+        } catch (error) {
+            console.error('Error removing old Message:', error);
+        }
+    })
 });
 
 server.listen(3000, () => {
